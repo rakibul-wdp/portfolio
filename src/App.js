5a805416949867a35731e79c6827490bfa9e1f21
rakibul-wdp <rakibul.wdp@gmail.com>
2022-06-12 01:40:45 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
