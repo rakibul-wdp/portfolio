608b05b5d7fc6f236c76413be0bacf0a7fd5bb1f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-20 16:47:41 +0600

    create animation for pipe symbol

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../css/Navbar.css";
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  setTimeout(() => {
+    setIsVisible(!isVisible);
+  }, 1000);
+
   return (
     <div className="relative w-[100vw]">
       <div className="flex justify-center mt-[40px]">
         <ul className="list-none flex flex-row text-white text-[16px] md:text-[24px]">
           <li className="mx-[10px] text-[#5ff3d1] hover:text-white transition duration-300 ease-in-out">
-            <a href="#about">\\&nbsp; About Me</a>
+            <a href="#about">
+              <span className="font-bold animation-pipe">||</span>
+              &nbsp; About Me
+            </a>
           </li>
           <li className="mx-[10px] text-[#5ff3d1] hover:text-white transition duration-300 ease-in-out ">
-            <a href="#skills">\\&nbsp; Skills</a>
+            <a href="#skills">
+              <span className="font-bold animation-pipe">||</span>&nbsp; Skills
+            </a>
           </li>
           <li className="mx-[10px] text-[#5ff3d1] hover:text-white transition duration-300 ease-in-out ">
-            <a href="#timeline">\\&nbsp; Timeline</a>
+            <a href="#timeline">
+              <span className="font-bold animation-pipe">||</span>&nbsp;
+              Timeline
+            </a>
           </li>
         </ul>
       </div>
