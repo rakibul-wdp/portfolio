09f6e9916dd175139e2263394c6558af3aab842f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-12 09:31:35 +0600

    create experience header section without experienceCard

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,8 +11,25 @@ import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work");
