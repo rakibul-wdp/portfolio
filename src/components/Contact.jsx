adda30740d6bc64787278eb9546636b340d28059
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-20 10:54:47 +0600

    create contact page header

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
@@ -8,9 +8,22 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => { }
+
+  const handleSubmit = (e) => { }
+
   return (
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact");
