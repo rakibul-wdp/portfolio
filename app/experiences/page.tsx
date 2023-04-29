1c7b3121d72af2f91e4396823930899923618568
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-30 05:55:00 +0600

    create nav links constants

---
+++ app/experiences/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Experiences</div>;
+};
+
+export default Page;
---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    id: 1,
+    route: "/about",
+    label: "About",
+  },
+  {
+    id: 2,
+    route: "/",
+    label: "Experiences",
+  },
+  {
+    id: 3,
+    route: "/projects",
+    label: "Projects",
+  },
+  {
+    id: 4,
+    route: "/skills",
+    label: "Skills",
+  },
+  {
+    id: 5,
+    route: "/timeline",
+    label: "Timeline",
+  },
+  {
+    id: 6,
+    route: "/blog",
+    label: "Blog",
+  },
+];
