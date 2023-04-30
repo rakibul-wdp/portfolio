684970aad7f5e04c97c2c3fccbeeeb5ab60ad90e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-01 04:55:00 +0600

    create container component to center

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,26 +3,31 @@
 import { navLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Container from "./Container";
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="flex items-center justify-around mt-5 text-2xl font-semibold text-[#5ff3d1] mb-14">
-      {navLinks.map((navLink) => {
-        const isActive = pathname === navLink.route;
+    <Container>
+      <nav className="flex items-center justify-around mt-5 text-2xl font-semibold text-[#5ff3d1] mb-14">
+        {navLinks.map((navLink) => {
+          const isActive = pathname === navLink.route;
 
-        return (
-          <Link
-            className={isActive ? "italic underline decoration-[#5ff3d1]" : ""}
-            href={navLink.route}
-            key={navLink.id}
-          >
-            {navLink.label}
-          </Link>
-        );
-      })}
-    </nav>
+          return (
+            <Link
+              className={
+                isActive ? "italic underline decoration-[#5ff3d1]" : ""
+              }
+              href={navLink.route}
+              key={navLink.id}
+            >
+              {navLink.label}
+            </Link>
+          );
+        })}
+      </nav>
+    </Container>
   );
 };
 
