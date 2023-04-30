b85118d67fd5087a3a097c2d30b63742046b6db7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-01 04:25:00 +0600

    implement active link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import { navLinks } from "@/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav>
-      {navLinks.map((navLink) => (
-        <Link href={navLink.route}>{navLink.label}</Link>
-      ))}
+    <nav className="flex items-center justify-around mt-5 text-2xl font-semibold text-[#5ff3d1] mb-14">
+      {navLinks.map((navLink) => {
+        const isActive = pathname === navLink.route;
+
+        return (
+          <Link
+            className={isActive ? "italic underline decoration-[#5ff3d1]" : ""}
+            href={navLink.route}
+            key={navLink.id}
+          >
+            {navLink.label}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
