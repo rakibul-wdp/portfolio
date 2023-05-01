18cda8f81d13f0f8be21dd641ab850fccd41be73
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-02 05:05:00 +0600

    create skills data

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,5 +1,23 @@
+import { experiences } from "@/constants/experiences";
+
 const Page = () => {
-  return <div className="text-white">Skills</div>;
+  return (
+    <div className="w-[85%]">
+      {experiences.map((experience) => (
+        <div
+          className="flex flex-col mb-10 text-white p-5 border-[0.01px] rounded-sm border-gray-500"
+          key={experience.id}
+        >
+          <h3>Programming Languages</h3>
+          <ul className="mt-7 list-disc list-inside">
+            {experience.descriptions.map((des) => (
+              <li key={des.id}>{des.des}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
---
+++ constants/skills.ts
@@ -0,0 +1,26 @@
+export const skills = [
+  {
+    id: 1,
+    title: "Programming Languages",
+    list: ["JavaScript, TypeScript, Python, C, C++, HTML/CSS"],
+  },
+  {
+    id: 2,
+    title: "Frameworks and Libraries",
+    list: ["ReactJS, NextJS, NodeJS, ExpressJS, NestJS, Mongoose, etc"],
+  },
+  {
+    id: 3,
+    title: "Design",
+    list: [
+      "CSS, TailwindCSS, MUI, Bootstrap, Chakra UI, Styled Components, etc",
+    ],
+  },
+  {
+    id: 4,
+    title: "Databases, Tools and Platform",
+    list: [
+      "MongoDB, NoSQL, Git, GitHub, GitLab, Bitbucket, Netlify, Heroku, Vercel, Figma, etc",
+    ],
+  },
+];
