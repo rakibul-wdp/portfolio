d0219b5ae0e98bb7574c934b556e687248706509
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-06 04:05:00 +0600

    update contact link with actual link

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 import { BsTwitter } from "react-icons/bs";
 import { FaFacebookSquare } from "react-icons/fa";
@@ -11,6 +12,7 @@ import { SlPhone } from "react-icons/sl";
 const contacts = [
   {
     id: 1,
+    link: "https://drive.google.com/file/d/1l-L8IO0F3ExL4EbyDMYFVZlku5840MZz/view?usp=sharing",
     label: "rakibul-resume",
     icon: <RxResume className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
@@ -29,30 +31,35 @@ const contacts = [
   },
   {
     id: 4,
+    link: "https://github.com/rakibul-wdp",
     label: "github/rakibul-wdp",
     icon: <AiOutlineGithub className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
   },
   {
     id: 5,
+    link: "https://leetcode.com/rakibul-wdp",
     label: "leetcode/rakibul-wdp",
     icon: <SiLeetcode className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
   },
   {
     id: 6,
+    link: "https://www.linkedin.com/in/rakibul-wdp",
     label: "linkedin/rakibul-wdp",
     icon: <AiFillLinkedin className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
   },
   {
     id: 7,
+    link: "https://twitter.com/rakibul_wdp",
     label: "twitter/rakibul_wdp",
     icon: <BsTwitter className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
   },
   {
     id: 8,
+    link: "https://www.facebook.com/rakibu.wdp",
     label: "facebook/rakibu.wdp",
     icon: <FaFacebookSquare className="mr-3 text-[#5ff3d1]" />,
     subIcon: <FiExternalLink className="text-[#5ff3d1]" />,
@@ -63,7 +70,9 @@ const Contact = () => {
   return (
     <div className="w-full">
       {contacts.map((contact) => (
-        <div
+        <Link
+          target={contact.link ? "_blank" : "_self"}
+          href={contact.link ? contact.link : ""}
           key={contact.id}
           className={`flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm ${
             contact.id % 2 === 0 ? "my-2" : ""
@@ -74,7 +83,7 @@ const Contact = () => {
             <span>{contact.label}</span>
           </div>
           {contact.subIcon}
-        </div>
+        </Link>
       ))}
     </div>
   );
