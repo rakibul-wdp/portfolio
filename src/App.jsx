40ebe4dc327fa7063fdaf381306a1dd99740fc29
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-07 17:46:35 +0600

    new idea and start work for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  StarsCanvas,
-  Tech,
-  Works,
-} from "./components";
+import './App.css'
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-};
+    <>
+      <h1>My Simple Portfolio</h1>
+    </>
+  )
+}
 
-export default App;
+export default App
