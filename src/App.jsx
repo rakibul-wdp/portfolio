9c05b0ec0c6e3f1aff4f9344525f474edc19ab65
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-07 19:15:48 +0600

    setup space and bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1>My Simple Portfolio</h1>
-    </>
+    <div className="grid lg:grid-cols-12 text-center text-white">
+      <h1 className="col-span-3">First</h1>
+      <h1 className="col-span-6">Second</h1>
+      <h1 className="col-span-3">Third</h1>
+    </div>
   )
 }
 
