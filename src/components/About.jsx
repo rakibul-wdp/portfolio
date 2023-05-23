2f8a802b64732e235226fa99042e8a66143da3e2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-23 18:47:23 +0600

    add contact boilarplate

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
     <div className='col-span-3 mt-36'>
       <img className='w-28 h-28 rounded-full mx-auto' src={Rakibul} alt="Md. Rakibul Islam" />
       <h1 className='text-3xl font-bold my-3 AnimatedGradientText'>Md. Rakibul Islam</h1>
-      <h2 className='text-2xl text-[#5ff3d1] text-start ml-8'>Full Stack Developer</h2>
-      <h2 className='text-2xl text-[#5ff3d1] my-1 text-start ml-16'>Programmer</h2>
-      <h2 className='text-2xl text-[#5ff3d1] text-start ml-24'>Problem Solver</h2>
+      <h2 className='text-2xl text-[#5ff3d1]'>Full Stack Developer</h2>
+      <h2 className='text-2xl text-[#5ff3d1]'>Programmer and</h2>
+      <h2 className='text-2xl text-[#5ff3d1]'>Problem Solver</h2>
       <p className='mt-5'>Myself Md. Rakibul Islam. I like to write and see code. I'm a full-stack developer with MERN stack technologies. I have four internship experiences as a full-stack developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur odit, est quas, eum, maiores quod esse aut officia expedita placeat tempora delectus tempore tenetur corporis animi! Odio sequi laboriosam cumque.</p>
     </div>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { RxResume } from "react-icons/rx";
+import { FiExternalLink } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import { MdContentCopy } from "react-icons/md";
+import { SlPhone } from "react-icons/sl";
+import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+import { SiLeetcode } from "react-icons/si";
+import { BsTwitter } from "react-icons/bs";
+import { FaFacebookSquare } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div className='col-span-3 bg-pink-600'>Contact</div>
+    <div className='col-span-3 mt-36 text-start ml-5'>
+      <div className='flex items-center justify-between text-xl font-bold px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <RxResume />
+        rakibul-resume
+        <FiExternalLink />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <HiOutlineMail />
+        Email
+        <MdContentCopy />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <SlPhone />
+        Phone No
+        <MdContentCopy />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <AiOutlineGithub />
+        GitHub
+        <FiExternalLink />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <SiLeetcode />
+        LeetCode
+        <FiExternalLink />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <AiFillLinkedin />
+        LinkedIn
+        <FiExternalLink />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <BsTwitter />
+        Twitter
+        <FiExternalLink />
+      </div>
+      <div className='flex items-center justify-between px-2 py-1 border-2 border-[#5ff3d1] rounded-sm'>
+        <FaFacebookSquare />
+        Facebook
+        <FiExternalLink />
+      </div>
+    </div>
   )
 }
 
