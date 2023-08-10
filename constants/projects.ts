7973a912209326c59fe05d4adce2997d3730e6d7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-10 12:41:30 +0600

    create projects data

---
+++ constants/projects.ts
@@ -0,0 +1,230 @@
+export const projects = [
+  {
+    id: 1,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 2,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 3,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 4,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 5,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 6,
+    images: [
+      {
+        id: 1,
+        img: "",
+      },
+      {
+        id: 2,
+        img: "",
+      },
+      {
+        id: 3,
+        img: "",
+      },
+    ],
+    projectName: "Fiverr",
+    projectStack: "Full Stack",
+    gitHub: "",
+    preview: "",
+    start: "Jun 2023",
+    end: "Current",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+    technologies: ["React", "NodeJS", "MongoDB"],
+  },
+];
