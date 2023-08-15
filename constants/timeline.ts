0a3cf83ca81b33c4d9b51c73b54bfa791f93d0af
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-15 09:54:30 +0600

    create timeline data model

---
+++ constants/timeline.ts
@@ -0,0 +1,62 @@
+export const timelines = [
+  {
+    id: 1,
+    title: "Join Programming Hero",
+    start: "Jan 2022",
+    end: "Mar 2022",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Join Learn with Sumit",
+    start: "Jan 2022",
+    end: "Mar 2022",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Join MetaStart",
+    start: "Jan 2022",
+    end: "Mar 2022",
+    descriptions: [
+      {
+        id: 1,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet. Vitae nostrum accusantium doloremque rerum explicabo assumenda soluta et minima maiores. Ipsum sunt blanditiis maxime distinctio quam alias sapiente ullam velit harum!",
+      },
+      {
+        id: 2,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+      {
+        id: 3,
+        des: "Lorem ipsum dolor sit amet, consectetur adipisicing. Lorem ipsum dolor sit amet.",
+      },
+    ],
+  },
+];
