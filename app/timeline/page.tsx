ffc22dd2c10c15fc443b34e2184528ae376db7ef
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-15 09:49:55 +0600

    create timeline structure

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,5 +1,41 @@
+import { experiences } from "@/constants/experiences";
+import { TiArrowUpThick } from "react-icons/ti";
+
 const Page = () => {
-  return <div className="text-white">TimeLine</div>;
+  return (
+    <div className="w-[85%]">
+      {experiences.map((experience) => (
+        <div
+          className="flex flex-col mb-10 text-white p-5 border-[0.01px] rounded-sm border-gray-500"
+          key={experience.id}
+        >
+          <div className="flex items-center justify-between">
+            <div className="w-full flex items-center justify-between">
+              <div className="h-[70px] flex flex-col justify-between">
+                <h1 className="text-2xl">
+                  {experience.role}{" "}
+                  <span className="italic text-sm">
+                    {experience.designation}
+                  </span>
+                </h1>
+                <h1 className="text-xl">{experience.companyName}</h1>
+              </div>
+              <div className="h-[70px] flex flex-col items-center justify-between">
+                <p>{experience.end}</p>
+                <TiArrowUpThick />
+                <p>{experience.start}</p>
+              </div>
+            </div>
+          </div>
+          <ul className="mt-7 list-disc list-inside">
+            {experience.descriptions.map((des) => (
+              <li key={des.id}>{des.des}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
