47287b12a213f80eda378c4872e86b14560ddc2a
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-26 17:17:17 +0600

    change navbar style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="mt-5 text-2xl font-semibold text-[#5ff3d1] mb-8 pb-6 sticky top-0">
-      <div className="lg:hidden flex items-center justify-center gap-5">
+    <nav className="mt-5 text-xl text-[#5ff3d1] mb-8 pb-6 sticky top-0">
+      <div className="flex items-center justify-center gap-10">
         {navLinks.map((navLink) => {
           const isActive = pathname === navLink.route;
 
@@ -17,27 +17,7 @@ const Navbar = () => {
             <Link
               className={
                 isActive
-                  ? "italic underline underline-offset-2 decoration-1 decoration-[#5ff3d1]"
-                  : ""
-              }
-              href={navLink.route}
-              key={navLink.id}
-            >
-              {navLink.label}
-            </Link>
-          );
-        })}
-      </div>
-
-      <div className="hidden lg:flex items-center justify-center gap-10">
-        {navLinks.slice(1).map((navLink) => {
-          const isActive = pathname === navLink.route;
-
-          return (
-            <Link
-              className={
-                isActive
-                  ? "italic underline underline-offset-2 decoration-1 decoration-[#5ff3d1]"
+                  ? "font-medium underline underline-offset-4 decoration-1 decoration-[#5ff3d1]"
                   : ""
               }
               href={navLink.route}
--- constants/navLinks.ts
+++ constants/navLinks.ts
@@ -1,41 +1,36 @@
 export const navLinks = [
   {
     id: 1,
-    route: "/about",
-    label: "About/Contact",
-  },
-  {
-    id: 2,
     route: "/",
     label: "Experiences",
   },
   {
-    id: 3,
+    id: 2,
     route: "/testimonials",
     label: "Testimonials",
   },
   {
-    id: 4,
+    id: 3,
     route: "/projects",
     label: "Projects",
   },
   {
-    id: 5,
+    id: 4,
     route: "/skills",
     label: "Skills",
   },
   {
-    id: 6,
+    id: 5,
     route: "/timeline",
     label: "Timeline",
   },
   {
-    id: 7,
+    id: 6,
     route: "/connect",
     label: "Connect",
   },
   {
-    id: 8,
+    id: 7,
     route: "/blog",
     label: "Blog",
   },
